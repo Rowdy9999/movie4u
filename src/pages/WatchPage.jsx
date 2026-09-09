5321aab6e3ed95a20e0249822523b02351a36130
Movie4u <movie4u@users.noreply.github.com>
2026-09-09 14:34:25 +0530

    fix: switch streaming provider to cinesrc.st (vidsrc.cc blocked iframe embedding)

--- src/pages/WatchPage.jsx
+++ src/pages/WatchPage.jsx
@@ -90,7 +90,7 @@ export default function WatchPage({ movieId, onNavigate }) {
 
       <div className="player-container">
         <iframe
-          src={`https://vidsrc.cc/v2/embed/movie/${movieId}`}
+          src={`https://cinesrc.st/embed/movie/${movieId}`}
           className="player-iframe"
           frameBorder="0"
           allowFullScreen
